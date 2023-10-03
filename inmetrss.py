cd9f1fcd20c79041904326e63002e2c3bf59e264
GabrielRF <github@gabrf.com>
2023-10-03 11:36:27 -0300

    Alertas Inmet

---
+++ inmetrss.py
@@ -0,0 +1,62 @@
+import base
+import datetime
+import feedparser
+import requests
+import os
+import telebot
+import xmltodict
+from telebot import types
+
+def get_alerts():
+    feed = feedparser.parse('https://apiprevmet3.inmet.gov.br/avisos/rss')
+    for item in feed['items']:
+        alert = {}
+        if 'Distrito Federal' not in str(item['description']):
+            continue
+        alert['title'] = item['title']
+        response = requests.get(item['link'])
+        data = xmltodict.parse(response.content)
+        creation = data['alert']['info']['onset']
+        creation = datetime.datetime.strptime(creation, '%Y-%m-%dT%H:%M:%S%z')
+        alert['creation'] = datetime.datetime.strftime(creation, '%d/%m/%Y %H:%M')
+        expires = data['alert']['info']['expires']
+        expires = datetime.datetime.strptime(expires, '%Y-%m-%dT%H:%M:%S%z')
+        alert['expires'] = datetime.datetime.strftime(expires, '%d/%m/%Y %H:%M')
+        alert['description'] = data['alert']['info']['description']
+        alert['instructions'] = data['alert']['info']['instruction']
+        alert['event'] = data['alert']['info']['event'] 
+        alert['link'] = data['alert']['info']['web']
+        if creation.date() == datetime.datetime.today().date():
+            return alert
+        return False
+
+def create_message(alert):
+    message = (
+        f'🌤 <b>INMET Alerta</b>\n\n'
+        f'❗️ <code>{alert["event"].title()}</code>\n' +
+        f'Início: {alert["creation"]}\n' +
+        f'Fim: {alert["expires"]}\n\n' +
+        f'⚠️ <i>{alert["description"]}</i>\n\n' +
+        f'ℹ️ {alert["instructions"]}'
+    )
+    return message
+
+def send_message(message, alert):
+    bot = telebot.TeleBot(os.environ.get(f'BOT_TOKEN'))
+    btn_link = types.InlineKeyboardMarkup()
+    btn = types.InlineKeyboardButton(f'🌤 Ver Alerta', url=alert['link'])
+    btn_link.row(btn)
+    bot.send_message(
+        os.environ.get(f'DESTINATION'),
+        message,
+        reply_markup=btn_link,
+        parse_mode='HTML'
+    )
+
+if __name__ == "__main__":
+    alert = get_alerts()
+    if alert:
+        if base.check_history(alert['link']):
+            exit()
+        send_message(create_message(alert), alert)
+        base.add_to_history(alert['link'])
