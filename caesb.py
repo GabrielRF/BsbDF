680eba9c2e25033f4a9a61b51bc82b90970900ee
Gabriel RF <gabriel.ferreira@avidity.se>
2023-10-18 18:57:12 -0300

    Usa Instant View do Telegram em vez de Telegra.ph

--- caesb.py
+++ caesb.py
@@ -3,6 +3,7 @@ import os
 import requests
 import telebot
 import telegraph
+import urllib
 from bs4 import BeautifulSoup
 
 def get_news_list(number, attempt=0):
@@ -21,32 +22,7 @@ def get_news_list(number, attempt=0):
 def get_news_content(html):
     div_content = html.find('div', {'class': 'item-page'})
     title = div_content.find('h2').text.strip()
-    content = div_content.text.strip().replace(title, '').replace('\t', '')
-    text = []
-    for line in content.split('\n'):
-        if len(line) > 2:
-            text.append(line)
-    subtitle = text[0]
-    text.pop(0)
-    full_text = ''
-    for p in text:
-        full_text = f'{full_text}<br><br>{p}'
-    return title, subtitle, full_text
-
-def create_telegraph_post(title, subtitle, full_text, link):
-    telegraph_auth = telegraph.Telegraph(
-        access_token=os.environ.get(f'TELEGRAPH_TOKEN')
-    )
-    response = telegraph_auth.create_page(
-        f'{title}',
-        html_content=(
-            f'<h4>{subtitle}</h4><br><br>' +
-            f'{full_text}<br><br>' +
-            f'<a href="https://www.caesb.df.gov.br/{link}">Leia a matéria original</a>'
-        ),
-        author_name=f'CAESB'
-    )
-    return response["url"]
+    return title
 
 def send_message(title, iv_link, link):
     bot = telebot.TeleBot(os.environ.get(f'BOT_TOKEN'))
@@ -66,9 +42,10 @@ if __name__ == "__main__":
     html = get_news_list(number)
     if not html:
         exit()
-    title, subtitle, full_text = get_news_content(html)
+    title = get_news_content(html)
     f = open('last_caesb.txt', 'w')
     f.write(str(number+1))
     f.close()
-    iv_link = create_telegraph_post(title, subtitle, full_text, number)
+    link = f'https://www.caesb.df.gov.br/{number}'
+    iv_link = f'https://t.me/iv?url={urllib.parse.quote_plus(link)}&rhash=fcdff120dfd9b9'
     send_message(title, iv_link, number)
