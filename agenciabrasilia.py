0928cf58831b66a7eb4cf666ffa46c37b7cbebf2
Gabriel RF <gabriel.ferreira@avidity.se>
2024-04-30 10:36:39 -0300

    Agencia Brasilia

--- agenciabrasilia.py
+++ agenciabrasilia.py
@@ -1,12 +1,9 @@
 import base
-import io
 import os
 import requests
 import telebot
-import telegraph
-import time
+import urllib
 from bs4 import BeautifulSoup
-from dotenv import load_dotenv
 
 def get_news_list():
     try:
@@ -16,60 +13,6 @@ def get_news_list():
     if response.status_code != 200:
         return False
     return BeautifulSoup(response.content, 'html.parser')
-
-def get_news_content(link):
-    print(f'Noticia {link}')
-    time.sleep(20)
-    response = requests.get(
-        link,
-        timeout=30,
-        headers={'User-agent': 'Mozilla/5.1'}
-    )
-    if response.status_code != 200:
-        return False
-    response = BeautifulSoup(response.content, 'html.parser')
-    content = response.find('div', {'id': 'content'})
-    author = response.find('p', {'class': 'by'}).text
-    text_content = response.findAll('p')
-    image = response.find('meta', {'property': 'og:image'})['content']
-    image = upload_telegraph_image(image)
-    subtitle = text_content[1]
-    full_text = ''
-    text_content.pop(1)
-    for p in text_content[2:]:
-        full_text = f'{full_text}<br><br>{p.text}'
-    return subtitle.text, full_text, image, author
-
-def upload_telegraph_image(image, attempt=0):
-    if attempt == 3:
-        return None
-    telegraph_auth = telegraph.Telegraph(
-        access_token=os.environ.get(f'TELEGRAPH_TOKEN')
-    )
-    time.sleep(20)
-    try:
-        file = requests.get(image)
-    except:
-        upload_telegraph_image(image, attempt=attempt+1)
-    inmemoryfile = io.BytesIO(file.content)
-    path = telegraph_auth.upload_file(inmemoryfile)
-    return f'https://telegra.ph{path[0]["src"]}'
-
-def create_telegraph_post(title, subtitle, full_text, link, image, author):
-    telegraph_auth = telegraph.Telegraph(
-        access_token=os.environ.get(f'TELEGRAPH_TOKEN')
-    )
-    response = telegraph_auth.create_page(
-        f'{title}',
-        html_content=(
-            f'<img src="{image}"><br><br>' +
-            f'<h4>{subtitle}</h4><br><br>' +
-            f'{full_text}<br><br>' +
-            f'<a href="{link}">Leia a matéria original</a>'
-        ),
-        author_name=f'{author}'
-    )
-    return response["url"]
 
 def send_message(title, iv_link, link):
     bot = telebot.TeleBot(os.environ.get(f'BOT_TOKEN'))
@@ -86,23 +29,18 @@ def send_message(title, iv_link, link):
     )
 
 if __name__ == "__main__":
-    load_dotenv()
     html = get_news_list()
     if not html:
         exit()
-    noticias = html.find('section', {'class': 'news'})
-    for noticia in noticias.findAll('div', {'class': 'col'})[:5]:
+    noticias = html.find('div', {'class': 'container'})
+    for noticia in noticias.findAll('div', {'class': 'col-md-12'})[:5]:
         try:
             link = noticia.find('a')['href']
         except TypeError:
             continue
         if base.check_history(link):
             continue
-        title = noticia.find('h2').text
-        try:
-            subtitle, full_text, image, author = get_news_content(link)
-        except:
-            continue
-        iv_link = create_telegraph_post(title, subtitle, full_text, link, image, author)
+        title = noticia.find('h3').text
+        iv_link = f'https://t.me/iv?url={urllib.parse.quote_plus(link)}&rhash=60af523a41b39d'
         send_message(title, iv_link, link)
         base.add_to_history(link)
